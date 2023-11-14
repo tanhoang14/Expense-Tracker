f7987a998d14198951b8e84e83d0414a2c92b415
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2023-11-13 21:38:38 -0800

    test

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -13,14 +13,6 @@ var app = builder.Build();
 //Register Syncfusion license
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5bdExjWXtXcHRcR2Vc");
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
